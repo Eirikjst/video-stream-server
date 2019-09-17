771bdcbf46382b373b62a687788c53d7a742b1c1
eirikjst <eirikjst@ntnu.stud.no>
2019-09-17 15:22:06 +0200

    ajax post() for videostream

--- public/index.js
+++ public/index.js
@@ -4,11 +4,12 @@ $(document).ready(function () {
         type: 'GET',
         contentType: 'application/json',
         success: function (data) {
+            console.log(data)
             displayData(data);
             clickHandler();
         },
-        error: function (err) {
-            console.log(err);
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
         }
     });
 });
@@ -25,7 +26,7 @@ function makeList(data) {
     $.each(data, function (attribute, val) {
         //checks if file, adds path and size to <li> element if true
         if (val.type == "file") {
-            newList.push(`<li class='video_file' video-file-path="${val.path}" video-file-size="${val.size}">` + val.name);
+            newList.push(`<li class='video_file' video-file-path="${val.path}" video-file-size="${val.size}" video-file-extension="${val.extension}">` + val.name);
         } else {
             newList.push(`<li>` + val.name);
         }
@@ -44,6 +45,8 @@ function clickHandler() {
             //do something...
             console.log($(this).attr('video-file-size'))
             console.log($(this).attr('video-file-path'))
+            console.log($(this).attr('video-file-extension'))
+            startStream($(this).attr('video-file-size'), $(this).attr('video-file-path'), $(this).attr('video-file-extension'))
         }
         if(this.getElementsByTagName("ul")[0] !== undefined){
             if (this.getElementsByTagName("ul")[0].style.display == "block") {
@@ -51,6 +54,26 @@ function clickHandler() {
             } else {
                 $(this).children(":first").slideDown();
             }
+        }
+    });
+}
+
+//TODO: implement video tag in index.html and feed the stream to tag
+function startStream(size, path, extension){
+    $.ajax({
+        url: '/stream',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            video_file_size: size,
+            video_file_path: path,
+            video_file_extension: extension
+        }),
+        success: function() {
+            console.log('ok')
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.responseText);
         }
     });
 }
